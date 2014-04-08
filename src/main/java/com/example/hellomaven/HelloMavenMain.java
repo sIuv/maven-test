acdab917043070170780deb0234de1cbbc496bc9
sIuv <pontus@englunds.de>
2014-04-08 16:10:10 +0200

    Adding files

---
+++ src/main/java/com/example/hellomaven/HelloMavenMain.java
@@ -0,0 +1,14 @@
+package com.example.hellomaven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMavenMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger = LoggerFactory.getlogger(HelloMavenMain.class);
+		logger.info("Hello Maven!");
+	}
+
+}
